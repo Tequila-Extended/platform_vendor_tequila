ProjectBlaze/vendor_blaze

vendor: Add our ringtone and set as default
Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

audio/audio.mk
@@ -1,0 +1,24 @@
+# Copyright (C) 2020 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include Android 9 Material sounds instead of default AOSP ones
+$(call inherit-product, frameworks/base/data/sounds/AudioPackage14.mk)
+
+# 2019 Material product sounds (CC-BY 4.0)
+# Source:  https://material.io/design/sound/sound-resources.html
+LOCAL_PATH := vendor/blaze/audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/alarms/MorningAlarm.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/MorningAlarm.ogg \
+    $(LOCAL_PATH)/notifications/Pikachu.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Pikachu.ogg \
+    $(LOCAL_PATH)/ringtones/vibe.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/vibe.ogg

config/common.mk
@@ -24,6 +24,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
+# Audio
+$(call inherit-product, vendor/blaze/audio/audio.mk)
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/blaze/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
@@ -213,3 +216,7 @@ PRODUCT_COPY_FILES += \
 endif
 
 include vendor/blaze/config/version.mk
+
+# Sounds (default)
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.ringtone=vibe.ogg

Tequila-Extended/platform_vendor_tequila

audio/audio.mk
@@ -1,0 +1,24 @@
+# Copyright (C) 2020 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include Android 9 Material sounds instead of default AOSP ones
+$(call inherit-product, frameworks/base/data/sounds/AudioPackage14.mk)
+
+# 2019 Material product sounds (CC-BY 4.0)
+# Source:  https://material.io/design/sound/sound-resources.html
+LOCAL_PATH := vendor/infinity/audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/alarms/MorningAlarm.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/MorningAlarm.ogg \
+    $(LOCAL_PATH)/notifications/Pikachu.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Pikachu.ogg \
+    $(LOCAL_PATH)/ringtones/vibe.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/vibe.ogg

config/common.mk
@@ -40,6 +40,8 @@ endif
 # Disable lockscreen live wallpaper for media metadata on lockscreen to work
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.wm.debug.lockscreen_live_wallpaper=false
+# Audio
+$(call inherit-product, vendor/tequila/audio/audio.mk)
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
@@ -216,6 +218,7 @@ PRODUCT_PACKAGES += \
     Updater
 endif
 
+<<<<<<< HEAD
 $(call inherit-product, vendor/tequila/config/version.mk)
 
 # Inherit from GMS product config
@@ -229,3 +232,10 @@ endif
 
 # RRO Overlays
 $(call inherit-product, vendor/tequila/config/rro_overlays.mk)
+=======
+include vendor/infinity/config/version.mk
+
+# Sounds (default)
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.ringtone=vibe.ogg
+>>>>>>> 96de0f1e (vendor: Add our ringtone and set as default)