b288b2d4b7bbf3268a2f0bb7f6945b4e80a13ead
Abhay Singh Gill <abhaygill017@gmail.com>
2025-06-05 09:54:30 +0530

    overlay: Make a overlay for keybox
    Change-Id: Id40251c68dc23568c4ebf835c0e60f1bf048f1bf
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -11,6 +11,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 # Bootanimation
 $(call inherit-product, vendor/tequila/config/bootanimation.mk)
 
+$(call inherit-product-if-exists, vendor/lineage/config/tequila.mk)
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
